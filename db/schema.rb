dc90c2b7eacdc93bd38f885fc0e98f8415f58f1f
“Angelica <angelicambanks@gmail.com>
2015-04-02 19:28:05 -0400

    added notes to database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150402172726) do
+ActiveRecord::Schema.define(version: 20150402231627) do
 
   create_table "todo_items", force: :cascade do |t|
     t.string   "content"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20150402172726) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.datetime "completed_at"
+    t.text     "notes"
   end
 
   add_index "todo_items", ["todo_list_id"], name: "index_todo_items_on_todo_list_id"
